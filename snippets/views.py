b043833f8187c5be508417ea7025cb30a15a66aa
healingfields <dihratene@gmail.com>
2022-03-07 16:14:24 +0100

    views using mixins

--- snippets/views.py
+++ snippets/views.py
@@ -1,15 +1,23 @@
+# django imports
+from os import remove
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+# app imports
 from .models import Snippet
 from .serializers import SnippetSerializer
-from django.shortcuts import get_object_or_404
+
+# DRF imports
+from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import Http404
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 
 
 # Create your views here.
@@ -131,3 +139,32 @@ class SnippetDetailsUsingCBV(APIView):
     def delete(self, request, pk, format=None):
         self.get_object(pk).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SnippetListUsingMixins(
+    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
+):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class SnippetDetailsUsingMixins(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView,
+):
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.remove(request, *args, **kwargs)
