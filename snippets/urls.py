3c340909096c72b52d29359ce2c09da41ebfa477
healingfields <dihratene@gmail.com>
2022-03-07 15:26:05 +0100

    updated my views by using request and respons provided by drf

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,19 @@
-from urllib.parse import urlparse
 from django.urls import path
-from .views import snippet_details, snippet_list
+from .views import (
+    snippet_details,
+    snippet_details_using_decorator,
+    snippet_list,
+    snippet_list_using_decorator,
+)
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path("snippets/", snippet_list, name="snippet_list"),
-    path("snippets/<int:pk>/", snippet_details, name="snippet_details"),
+    # path("snippets/", snippet_list, name="snippet_list"),
+    # path("snippets/<int:pk>/", snippet_details, name="snippet_details"),
+    path("snippets/", snippet_list_using_decorator, name="snippet_list"),
+    path("snippets/<int:pk>/", snippet_details_using_decorator, name="snippet_details"),
 ]
+
+# support for multiples formats
+urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,4 +1,3 @@
-from telnetlib import STATUS
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -6,6 +5,10 @@ from rest_framework.parsers import JSONParser
 from .models import Snippet
 from .serializers import SnippetSerializer
 from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
 @csrf_exempt
@@ -24,6 +27,21 @@ def snippet_list(request):
         return JsonResponse(serializer.errors, status=400)
 
 
+@api_view(["GET", "POST"])
+def snippet_list_using_decorator(request, format=None):
+    if request.method == "GET":
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = SnippetSerializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
 def snippet_details(request, pk):
     try:
         snippet = get_object_or_404(Snippet, pk=pk)
@@ -35,7 +53,7 @@ def snippet_details(request, pk):
         return JsonResponse(serializer.data)
 
     elif request.method == "PUT":
-        data = JSONParser.parse(request)
+        data = JSONParser().parse(request)
         serializer = SnippetSerializer(snippet, data=data)
         if serializer.is_valid():
             serializer.save()
@@ -45,3 +63,29 @@ def snippet_details(request, pk):
     elif request.method == "DELETE":
         snippet.delete()
         return HttpResponse(status=204)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+# the format param for rendering our response on multiples types
+def snippet_details_using_decorator(request, pk, format=None):
+    try:
+        snippet = get_object_or_404(Snippet, pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        # request.data can handle incoming json request and other formats
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        # Response is returning the correct content type for us
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
