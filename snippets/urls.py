5c6e60ea57d6cc4010f75b40cbd3b553873a27cc
healingfields <dihratene@gmail.com>
2022-03-13 01:33:40 +0100

    used routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,4 @@
-from unicodedata import name
-from django.urls import path
+from django.urls import path, include
 from .views import (
     SnippetDetailsUsingGenericCBV,
     SnippetHighlight,
@@ -18,16 +17,22 @@ from .views import (
 )
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 
-snippet_list = SnippetViewSet.as_view({"get": "list", "post": "create"})
-snippet_detail = SnippetViewSet.as_view(
-    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
-)
-snippet_highlight = SnippetViewSet.as_view(
-    {"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer]
-)
-user_list = UserViewSet.as_view({"get": "list"})
-user_detail = UserViewSet.as_view({"get": "retrieve"})
+
+# snippet_list = SnippetViewSet.as_view({"get": "list", "post": "create"})
+# snippet_detail = SnippetViewSet.as_view(
+#     {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
+# )
+# snippet_highlight = SnippetViewSet.as_view(
+#     {"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer]
+# )
+# user_list = UserViewSet.as_view({"get": "list"})
+# user_detail = UserViewSet.as_view({"get": "retrieve"})
+
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet)
+router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
@@ -57,14 +62,15 @@ urlpatterns = [
     #     SnippetHighlight.as_view(),
     #     name="snippet-highlight",
     # ),
-    path("", api_root),
-    path("snippets/", snippet_list, name="snippet-list"),
-    path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
-    path("snippets/<int:pk>/highlight/", snippet_highlight, name="snippet-highlight"),
-    path("users/", user_list, name="user-list"),
-    path("users/<int:pk>/", user_detail, name="user-detail"),
+    # path("", api_root),
+    # path("snippets/", snippet_list, name="snippet-list"),
+    # path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
+    # path("snippets/<int:pk>/highlight/", snippet_highlight, name="snippet-highlight"),
+    # path("users/", user_list, name="user-list"),
+    # path("users/<int:pk>/", user_detail, name="user-detail"),
+    path('', include(router.urls))
 ]
 
 
 # support for multiples formats
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
